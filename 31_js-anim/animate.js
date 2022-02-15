a89f35a90dc2ec20262adda6523cf9e1c9750448
Ivan Lam <ilam21@stuy.edu>
2022-02-15 09:26:36 -0500

    commit number1

---
+++ 31_js-anim/animate.js
@@ -0,0 +1,84 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
+// SoftDev pd0
+// K31 -- canvas based JS animation
+// 2022-02-15t
+
+// model for HTML5 canvas-based animation
+
+// SKEELTON
+
+
+//access canvas and buttons via DOM
+var c = document.getElementById("playground");// GET CANVAS
+var dotButton = document.getElementById("buttonCircle");// GET DOT BUTTON
+var stopButton = document.getElementById("buttonStop");// GET STOP BUTTON
+
+//prepare to interact with canvas in 2D
+var ctx = c.getContext("2d");// YOUR CODE HERE
+
+//set fill color to team color
+ctx.fillStyle = '#75E6DA' // YOUR CODE HERE
+
+var requestID;  //init global var for use with animation frames
+
+
+//var clear = function(e) {
+var clear = (e) => {
+  console.log("clear invoked...")
+  ctx.clearRect(0, 0, playground.width, playground.height);
+  // YOUR CODE HERE
+};
+
+
+var radius = 0;
+var growing = true;
+
+
+//var drawDot = function() {
+var drawDot = () => {
+  console.log("drawDot invoked...")
+  clear;
+  ctx.arc((playground.width)/2, (playground.height)/2, radius, 2 * Math.PI, false);
+  ctx.fill();
+  ctx.stroke();
+
+  if (growing) {
+    radius += 1;
+  } else {
+    radius -= 1;
+  }
+  // YOUR CODE HERE
+
+  /*
+    ...to
+    Wipe the canvas,
+    Repaint the circle,
+
+    ...and somewhere (where/when is the right time?)
+    Update requestID to propagate the animation.
+    You will need
+    window.cancelAnimationFrame()
+    window.requestAnimationFrame()
+
+   */
+};
+
+
+//var stopIt = function() {
+var stopIt = () => {
+  console.log("stopIt invoked...")
+  console.log( requestID );
+
+  // YOUR CODE HERE
+  /*
+    ...to
+    Stop the animation
+
+    You will need
+    window.cancelAnimationFrame()
+  */
+};
+
+
+dotButton.addEventListener( "click", drawDot );
+stopButton.addEventListener( "click",  stopIt );
