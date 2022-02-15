852edd59360fe84f103cc852bd2fde307409cfa3
Ivan Lam <ilam21@stuy.edu>
2022-02-14 22:06:56 -0500

    coded k30

--- k30/clickdraw.js
+++ k30/clickdraw.js
@@ -1,33 +1,60 @@
-var c = Document.getElementById("slate");
+var c = document.getElementById("slate")
 
-var ctx = c.gotContext("2d");
+var ctx = c.getContext("2d");
+
 
 var mode = "rect";
 
+
 var toggleMode = (e) => {
-	console.log("togging...");
-	if (mode == "rect") {
+	if (mode === "rect") {
 		mode = "circ";
+		document.getElementById('buttonToggle').innerHTML = "Circle";
 	} else {
-		mode = "rect"
+		mode = "rect";
+		document.getElementById('buttonToggle').innerHTML = "Rectangle";
 	}
 }
 
 var drawRect = function(e) {
-	var mouseX = e.clientX;
-	var mouseY = e.clinetY;
+	var mouseX = e.offsetX;
+	var mouseY = e.offsetY;
+	ctx.beginPath();
+	ctx.rect(mouseX, mouseY, 200, 200);
+	ctx.fillStyle = '#ff0000'; 
+	ctx.fill();
+	ctx.stroke();
 	console.log("mouseclick registered at ", mouseX, mouseY);
 }
 
+var drawCircle = (e) => {
+	var mouseX = e.offsetX;
+	var mouseY = e.offsetY;
+	ctx.beginPath();
+	ctx.arc(mouseX, mouseY, 50, 2 * Math.PI, false);
+	ctx.fillStyle = '#00ff00';
+	ctx.fill();
+	ctx.stroke();
+	console.log("mouseclick registered at ", mouseX, mouseY);
+	
+}
+
+
 var draw = (e) => {
-	if (mode == "rect") {
-		drawRect(); 
+	if (mode === "rect") {
+		drawRect(e);
 	} else {
-		drawCircle();
+		drawCircle(e);
 	}
 }
 
-var wipeCanvas = () => {
-	c.fill(#FFFFFF);
 
+var wipeCanvas = () => {
+	ctx.clearRect(0, 0, slate.width, slate.height);
 }
+
+c.addEventListener("click", draw);
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipeCanvas);
